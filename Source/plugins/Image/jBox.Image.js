0d79c9863c0092b1bc546ff6f92b439417bdae65
Stephan Wagner <stephanwagner.me@gmail.com>
2016-11-21 22:57:32 -0500

    Version 0.4.1

--- Source/plugins/Image/jBox.Image.js
+++ Source/plugins/Image/jBox.Image.js
@@ -1,185 +1,226 @@
 /*
----
-description: jBox Image plugin: Adds an image lightbox to your images
-
-authors: Stephan Wagner (http://stephanwagner.me)
-
-license: MIT (http://opensource.org/licenses/MIT)
-
-requires: jBox 0.4.0 (http://jboxcdn.com/jBox.min.js)
-		  jQuery 3.1.1 (https://code.jquery.com/jquery-3.1.1.min.js)
-...
-*/
+ * jBox Image plugin: Adds a lightbox to your images
+ *
+ * Author: Stephan Wagner (https://stephanwagner.me)
+ *
+ * License: MIT (http://opensource.org/licenses/MIT)
+ * 
+ * Requires: jBox 0.4.0 (https://code.jboxcdn.com/jBox.min.js)
+ *           jQuery 3.1.1 (https://code.jquery.com/jquery-3.1.1.min.js)
+ */
+ 
 jQuery(document).ready(function () {
-	
-	new jBoxPlugin('Image', {
-		
-		// Options
-		
-		src: 'href',				// The attribute where jBox gets the image source from, e.g. href="/path_to_image/image.jpg"
-		gallery: 'data-jbox-image',	// The attribute where you define the image gallery, e.g. data-jbox-image="gallery1"
-		imageLabel: 'title',		// The attribute where jBox gets the image label from, e.g. title="My label"
-		imageFade: 360,				// The fade duration for images
-		imageSize: 'contain',		// How to display the images: Use CSS background-position values, e.g. 'cover', 'contain', 'auto', 'initial', '50% 50%'
-		
-		attach: jQuery('[data-jbox-image]'),
-		target: jQuery(window),
-		fixed: true,
-		blockScroll: true,
-		closeOnEsc: true,
-		closeOnClick: 'button',
-		closeButton: true,
-		overlay: true,
-		animation: 'zoomIn',
-		preventDefault: true,
-		width: 10000,
-		height: 10000,
-		adjustDistance: {
-			top: 5,
-			right: 5,
-			bottom: 40,
-			top: 40
-		},
-		
-		// Set up system methods
-		_onInit: function() {
-			this.images = this.currentImage = {};
-			this.imageZIndex = 1;
-			
-			// Loop through images, sort and save in global variable
-			this.attachedElements && jQuery.each(this.attachedElements, function (index, item) {
-				item = jQuery(item);
-				if (item.data('jBox-image-gallery')) return;
-				var gallery = item.attr(this.options.gallery) || 'default';
-				!this.images[gallery] && (this.images[gallery] = []);
-				this.images[gallery].push({src: item.attr(this.options.src), label: (item.attr(this.options.imageLabel) || '')});
-				
-				// Remove the title attribute so it won't show the browsers tooltip
-				this.options.imageLabel == 'title' && item.removeAttr('title');
-				
-				// Store data in source element for easy access
-				item.data('jBox-image-gallery', gallery);
-				item.data('jBox-image-id', (this.images[gallery].length - 1));
-			}.bind(this));
-			
-			// Helper to inject the image into content area
-			var appendImage = function(gallery, id, preload, open, error) {
-				
-				// Abort if image was appended already
-				if (jQuery('#jBox-image-' + gallery + '-' + id).length) return;
-				
-				// Create image container
-				var image = jQuery('<div/>', {
-					id: 'jBox-image-' + gallery + '-' + id,
-					'class': 'jBox-image-container' + (error ? ' jBox-image-not-found' : '')
-				}).css({
-					backgroundImage: error ? '' : 'url("' + this.images[gallery][id].src + '")',
-					backgroundSize: this.options.imageSize,
-					opacity: (open ? 1 : 0),
-					zIndex: (preload ? 0 : this.imageZIndex++)
-				}).appendTo(this.content);
-				
-				// Create labels
-				var text = jQuery('<div/>', {
-					id: 'jBox-image-label-' + gallery + '-' + id,
-					'class': 'jBox-image-label' + (open ? ' active' : '')
-				}).html(this.images[gallery][id].label).click(function() { $(this).toggleClass('expanded'); }).appendTo(this.imageLabel);
-				
-				!open && !preload && image.animate({opacity: 1}, this.options.imageFade);
-			}.bind(this);
-			
-			// Helper to show new image label
-			var showLabel = function(gallery, id) {
-				jQuery('.jBox-image-label.active').removeClass('active expanded');
-				jQuery('#jBox-image-label-' + gallery + '-' + id).addClass('active');
-			};
-			
-			// Show images when they are loaded or load them if not
-			this.showImage = function(img) {
-				// Get the gallery and the image id from the next or the previous image
-				if (img != 'open') {
-					var gallery = this.currentImage.gallery;
-					var id = this.currentImage.id + (1 * (img == 'prev') ? -1 : 1);
-					id = id > (this.images[gallery].length - 1) ? 0 : (id < 0 ? (this.images[gallery].length - 1) : id);
-					// Or get image data from source element
-				} else {
-					var gallery = this.source.data('jBox-image-gallery');
-					var id = this.source.data('jBox-image-id');
-					
-					// Remove or show the next and prev buttons
-					jQuery('.jBox-image-pointer-prev, .jBox-image-pointer-next').css({display: (this.images[gallery].length > 1 ? 'block' : 'none')});
-				}
-				
-				// Set new current image
-				this.currentImage = {gallery: gallery, id: id};
-				
-				// Show image if it already exists
-				if (jQuery('#jBox-image-' + gallery + '-' + id).length) {
-					jQuery('#jBox-image-' + gallery + '-' + id).css({zIndex: this.imageZIndex++, opacity: 0}).animate({opacity: 1}, (img == 'open') ? 0 : this.options.imageFade);
-					showLabel(gallery, id);
-					
-				// Load image
-				} else {
-					this.wrapper.addClass('jBox-image-loading');
-					
-				    jQuery('<img src="' + this.images[gallery][id].src + '"/>').each(function() {
-				        var tmpImg = new Image();
-				        tmpImg.onload = function() {
-					        appendImage(gallery, id, false);
-							showLabel(gallery, id);
-							this.wrapper.removeClass('jBox-image-loading');
-					    }.bind(this);
-				        tmpImg.onerror = function() {
-					        appendImage(gallery, id, false, null, true);
-							showLabel(gallery, id);
-							this.wrapper.removeClass('jBox-image-loading');
-					    }.bind(this);
-					    tmpImg.src = this.images[gallery][id].src;
-	    			}.bind(this));
-				}
-				
-				// Preload next image
-				var next_id = id + 1;
-				next_id = next_id > (this.images[gallery].length - 1) ? 0 : (next_id < 0 ? (this.images[gallery].length - 1) : next_id);
-				
-				(!jQuery('#jBox-image-' + gallery + '-' + next_id).length) && jQuery('<img src="' + this.images[gallery][next_id].src + '"/>').each(function() {
-			        var tmpImg = new Image();
-			        tmpImg.onload = function() {
-				        appendImage(gallery, next_id, true);
-				    }.bind(this);
-			        tmpImg.onerror = function() {
-				        appendImage(gallery, next_id, true, null, true);
-				    }.bind(this);
-					tmpImg.src = this.images[gallery][next_id].src;
-				}.bind(this));
-			};
-		},
-		_onCreated: function() {
-			this.imageLabel = jQuery('<div/>', {'class': 'jBox-image-label-container'}).appendTo(this.wrapper);
-			this.imageLabel.append(jQuery('<div/>', {'class': 'jBox-image-pointer-prev', click: function() { this.showImage('prev'); }.bind(this)})).append(jQuery('<div/>', {'class': 'jBox-image-pointer-next', click: function() { this.showImage('next'); }.bind(this)}));
-		},
-		_onOpen: function() {
-			// Add a class to body so you can control the appearance of the overlay, for images a darker one is better
-			jQuery('body').addClass('jBox-image-open');
-			
-			// Add key events
-			jQuery(document).on('keyup.jBox-Image-' + this.id, function(ev) {
-				(ev.keyCode == 37) && this.showImage('prev');
-				(ev.keyCode == 39) && this.showImage('next');
-			}.bind(this));
-			
-			// Load the image from the attached element
-			this.showImage('open');
-		},
-		_onClose: function() {
-			jQuery('body').removeClass('jBox-image-open');
-			
-			// Remove key events
-			jQuery(document).off('keyup.jBox-Image-' + this.id);
-		},
-		_onCloseComplete: function() {
-			// Hide all images
-			this.wrapper.find('.jBox-image-container').css('opacity', 0);
-		}
-	});
+  
+  new jBoxPlugin('Image', {
+    
+    
+    // Options (https://stephanwagner.me/jBox/options#options-confirm)
+    
+    src: 'href',                // The attribute where jBox gets the image source from, e.g. href="/path_to_image/image.jpg"
+    gallery: 'data-jbox-image', // The attribute where you define the image gallery, e.g. data-jbox-image="gallery1"
+    imageLabel: 'title',        // The attribute where jBox gets the image label from, e.g. title="My label"
+    imageFade: 360,             // The fade duration for images
+    imageSize: 'contain',       // How to display the images: Use CSS background-position values, e.g. 'cover', 'contain', 'auto', 'initial', '50% 50%'
+    target: jQuery(window),
+    attach: jQuery('[data-jbox-image]'),
+    fixed: true,
+    blockScroll: true,
+    closeOnEsc: true,
+    closeOnClick: 'button',
+    closeButton: true,
+    overlay: true,
+    animation: 'zoomIn',
+    preventDefault: true,
+    width: '100%',
+    height: '100%',
+    adjustDistance: {
+      top: 40,
+      right: 5,
+      bottom: 40,
+      left: 5
+    },
+    
+    
+    // Triggered when jBox is initialized
+    
+    _onInit: function ()
+    {
+      // Initial images and z-index
+      this.images = this.currentImage = {};
+      this.imageZIndex = 1;
+      
+      // Loop through images, sort and save in global variable
+      this.attachedElements && jQuery.each(this.attachedElements, function (index, item)
+      {
+        item = jQuery(item);
+        
+        // Abort if the item was added to a gallery already
+        if (item.data('jBox-image-gallery')) return;
+        
+        // Add item to a gallery
+        var gallery = item.attr(this.options.gallery) || 'default';
+        !this.images[gallery] && (this.images[gallery] = []);
+        this.images[gallery].push({src: item.attr(this.options.src), label: (item.attr(this.options.imageLabel) || '')});
+        
+        // Remove the title attribute so it won't show the browsers tooltip
+        this.options.imageLabel == 'title' && item.removeAttr('title');
+        
+        // Store data in source element for easy access
+        item.data('jBox-image-gallery', gallery);
+        item.data('jBox-image-id', (this.images[gallery].length - 1));
+        
+      }.bind(this));
+      
+      // Helper to inject the image into content area
+      var appendImage = function (gallery, id, preload, open, error)
+      {
+        // Abort if image was appended already
+        if (jQuery('#jBox-image-' + gallery + '-' + id).length) return;
+        
+        // Create image container
+        var image = jQuery('<div/>', {
+          id: 'jBox-image-' + gallery + '-' + id,
+          'class': 'jBox-image-container' + (error ? ' jBox-image-not-found' : '')
+        }).css({
+          backgroundImage: error ? '' : 'url("' + this.images[gallery][id].src + '")',
+          backgroundSize: this.options.imageSize,
+          opacity: (open ? 1 : 0),
+          zIndex: (preload ? 0 : this.imageZIndex++)
+        }).appendTo(this.content);
+        
+        // Create labels
+        var text = jQuery('<div/>', {
+          id: 'jBox-image-label-' + gallery + '-' + id,
+          'class': 'jBox-image-label' + (open ? ' active' : '')
+        }).html(this.images[gallery][id].label).click(function () { $(this).toggleClass('expanded'); }).appendTo(this.imageLabel);
+        
+        // Show image
+        !open && !preload && image.animate({opacity: 1}, this.options.imageFade);
+        
+      }.bind(this);
+      
+      // Helper to show new image label
+      var showLabel = function (gallery, id)
+      {
+        jQuery('.jBox-image-label.active').removeClass('active expanded');
+        jQuery('#jBox-image-label-' + gallery + '-' + id).addClass('active');
+      };
+      
+      // Show images when they are loaded or load them if not
+      this.showImage = function (img)
+      {
+        // Get the gallery and the image id from the next or the previous image
+        if (img != 'open') {
+          var gallery = this.currentImage.gallery;
+          var id = this.currentImage.id + (1 * (img == 'prev') ? -1 : 1);
+          id = id > (this.images[gallery].length - 1) ? 0 : (id < 0 ? (this.images[gallery].length - 1) : id);
+        
+        // Or get image data from source element
+        } else {
+          var gallery = this.source.data('jBox-image-gallery');
+          var id = this.source.data('jBox-image-id');
+          
+          // Remove or show the next and prev buttons
+          jQuery('.jBox-image-pointer-prev, .jBox-image-pointer-next').css({display: (this.images[gallery].length > 1 ? 'block' : 'none')});
+        }
+        
+        // Set new current image
+        this.currentImage = {gallery: gallery, id: id};
+        
+        // Show image if it already exists
+        if (jQuery('#jBox-image-' + gallery + '-' + id).length) {
+          jQuery('#jBox-image-' + gallery + '-' + id).css({zIndex: this.imageZIndex++, opacity: 0}).animate({opacity: 1}, (img == 'open') ? 0 : this.options.imageFade);
+          showLabel(gallery, id);
+          
+        // Load image
+        } else {
+          this.wrapper.addClass('jBox-image-loading');
+          
+          jQuery('<img src="' + this.images[gallery][id].src + '"/>').each(function ()
+          {
+            var tmpImg = new Image();
+            tmpImg.onload = function ()
+            {
+              appendImage(gallery, id, false);
+              showLabel(gallery, id);
+              this.wrapper.removeClass('jBox-image-loading');
+            }.bind(this);
+            
+            tmpImg.onerror = function () {
+              appendImage(gallery, id, false, null, true);
+              showLabel(gallery, id);
+              this.wrapper.removeClass('jBox-image-loading');
+            }.bind(this);
+            
+            tmpImg.src = this.images[gallery][id].src;
+          }.bind(this));
+        }
+        
+        // Preload next image
+        var next_id = id + 1;
+        next_id = next_id > (this.images[gallery].length - 1) ? 0 : (next_id < 0 ? (this.images[gallery].length - 1) : next_id);
+        
+        (!jQuery('#jBox-image-' + gallery + '-' + next_id).length) && jQuery('<img src="' + this.images[gallery][next_id].src + '"/>').each(function ()
+        {
+          var tmpImg = new Image();
+          tmpImg.onload = function ()
+          {
+              appendImage(gallery, next_id, true);
+          }.bind(this);
+          
+          tmpImg.onerror = function ()
+          {
+            appendImage(gallery, next_id, true, null, true);
+          }.bind(this);
+          
+          tmpImg.src = this.images[gallery][next_id].src;
+        }.bind(this));
+      };
+    },
+    
+    
+    // Triggered when jBox was created
+    
+    _onCreated: function ()
+    {
+      // Append image label containers
+      this.imageLabel = jQuery('<div/>', {'class': 'jBox-image-label-container'}).appendTo(this.wrapper);
+      this.imageLabel.append(jQuery('<div/>', {'class': 'jBox-image-pointer-prev', click: function () { this.showImage('prev'); }.bind(this)})).append(jQuery('<div/>', {'class': 'jBox-image-pointer-next', click: function () { this.showImage('next'); }.bind(this)}));
+    },
+    
+    
+    // Triggered when jBox opens
+    
+    _onOpen: function ()
+    {
+      // Add key events
+      jQuery(document).on('keyup.jBox-Image-' + this.id, function (ev) {
+        (ev.keyCode == 37) && this.showImage('prev');
+        (ev.keyCode == 39) && this.showImage('next');
+      }.bind(this));
+      
+      // Load the image from the attached element
+      this.showImage('open');
+    },
+    
+    
+    // Triggered when jBox closes
+    
+    _onClose: function ()
+    {
+      // Remove key events
+      jQuery(document).off('keyup.jBox-Image-' + this.id);
+    },
+    
+    
+    // Triggered when jBox finished closing
+    
+    _onCloseComplete: function ()
+    {
+      // Hide all image containers
+      this.wrapper.find('.jBox-image-container').css('opacity', 0);
+    }
+    
+  });
+  
 });
--- Source/plugins/Notice/jBox.Notice.js
+++ Source/plugins/Notice/jBox.Notice.js
@@ -1,113 +1,140 @@
 /*
----
-description: jBox Notice plugin: Opens a popup notice
-
-authors: Stephan Wagner (http://stephanwagner.me)
-
-license: MIT (http://opensource.org/licenses/MIT)
-
-requires: jBox 0.4.0 (http://jboxcdn.com/jBox.min.js)
-		  jQuery 3.1.1 (https://code.jquery.com/jquery-3.1.1.min.js)
-...
-*/
+ * jBox Notice plugin: Opens a popup notice
+ *
+ * Author: Stephan Wagner (https://stephanwagner.me)
+ *
+ * License: MIT (http://opensource.org/licenses/MIT)
+ *
+ * Requires: jBox 0.4.0 (https://code.jboxcdn.com/jBox.min.js)
+ *           jQuery 3.1.1 (https://code.jquery.com/jquery-3.1.1.min.js)
+ */
 
 jQuery(document).ready(function () {
-	
-	new jBoxPlugin('Notice', {
-		
-		// Options
-		
-		color: null,			// Add a color to your notices, use 'gray' (default), 'black', 'red', 'green', 'blue' or 'yellow'
-		stack: true,			// Set to false to disable notice-stacking
-		autoClose: 6000,		// Time in ms after which the notice will disappear
-		attributes: {			// Defines where the notice will pop up
-			x: 'right',			// 'left' or 'right'
-			y: 'top'			// 'top' or 'bottom'
-		},
-		position: {				// Defines the distance to the viewport boundary
-			x: 15,
-			y: 15
-		},
-		responsivePositions: {	// Responsive positions
-			500: {				// The key defines the maximum width of the viewport, the values will replace the default position options
-				x: 5,			// Start with the lowest viewport
-				y: 5
-			},
-			768: {
-				x: 10,
-				y: 10
-			}
-		},
-		target: jQuery(window),
-		fixed: true,
-		animation: 'zoomIn',
-		closeOnClick: 'box',
-		zIndex: 12000,
-		
-		// Set up system methods
-		_onInit: function () {
-			
-			// Cache position
-			this.defaultNoticePosition = jQuery.extend({}, this.options.position);
-			
-			// Type Notice has its own adjust position function
-			this._adjustNoticePositon = function () {
-				var win = jQuery(window);
-				var windowDimensions = {
-					x: win.width(),
-					y: win.height()
-				};
-				
-				// Reset default position
-				this.options.position = jQuery.extend({}, this.defaultNoticePosition);
-				
-				// Adjust depending on viewport
-				jQuery.each(this.options.responsivePositions, function (viewport, position) {
-					if (windowDimensions.x <= viewport) {
-						this.options.position = position;
-						return false;
-					}
-				}.bind(this));
-				
-				// Set new distances to viewport
-				this.options.adjustDistance = {
-					top: this.options.position.y,
-					right: this.options.position.x,
-					bottom: this.options.position.y,
-					left: this.options.position.x
-				};
-			};
-			
-			jQuery(window).on('resize.responsivejBoxNotice-' + this.id, function(ev) { if (this.isOpen) { this._adjustNoticePositon(); } }.bind(this));
-			
-			this.open();
-		},
-		_onCreated: function() {
-			this.wrapper.addClass('jBox-Notice-color jBox-Notice-' + (this.options.color || 'gray'));
-			this.wrapper.data('jBox-Notice-position', this.options.attributes.x + '-' + this.options.attributes.y);
-		},
-		_onOpen: function() {
-			// Adjust position when opening
-			this._adjustNoticePositon();
-			
-			// Loop through notices at same window corner and either move or destroy them
-			jQuery.each(jQuery('.jBox-Notice'), function(index, el) {
-				el = jQuery(el);
-				
-				if (el.attr('id') == this.id || el.data('jBox-Notice-position') != this.options.attributes.x + '-' + this.options.attributes.y) return;
-				if (!this.options.stack) {
-					el.data('jBox').close({ignoreDelay: true});
-					return;
-				}
-				var margin = (el.data('jBoxNoticeMargin') ? parseInt(el.data('jBoxNoticeMargin')) : parseInt(el.css('margin-' + this.options.attributes.y))) + this.wrapper.outerHeight() + (jQuery(window).width() > 500 ? 10 : 5);
-				el.data('jBoxNoticeMargin', margin);
-				el.css('margin-' + this.options.attributes.y, margin);
-			}.bind(this));
-		},
-		
-		// Remove notice from DOM when closing finishes
-		_onCloseComplete: function() {
-			this.destroy();
-		}
-	});
+  
+  new jBoxPlugin('Notice', {
+    
+    
+    // Options (https://stephanwagner.me/jBox/options#options-notice)
+    
+    color: null,      // Add a color to your notices, use 'gray' (default), 'black', 'red', 'green', 'blue' or 'yellow'
+    stack: true,      // Set to false to disable notice-stacking
+    autoClose: 6000,  // Time in ms after which the notice will disappear
+    attributes: {     // Defines where the notice will pop up
+      x: 'right',     // 'left' or 'right'
+      y: 'top'        // 'top' or 'bottom'
+    },
+    position: {       // Defines the distance to the viewport boundary
+      x: 15,
+      y: 15
+    },
+    responsivePositions: {  // Responsive positions
+      500: {                // The key defines the maximum width of the viewport, the values will replace the default position options
+        x: 5,               // Start with the lowest viewport
+        y: 5
+      },
+      768: {
+        x: 10,
+        y: 10
+      }
+    },
+    target: jQuery(window),
+    fixed: true,
+    animation: 'zoomIn',
+    closeOnClick: 'box',
+    zIndex: 12000,
+    
+    
+    // Triggered when notice is initialized
+    
+    _onInit: function ()
+    {
+      // Cache position values
+      this.defaultNoticePosition = jQuery.extend({}, this.options.position);
+      
+      // Type Notice has its own adjust position function
+      this._adjustNoticePositon = function () {
+        var win = jQuery(window);
+        var windowDimensions = {
+          x: win.width(),
+          y: win.height()
+        };
+        
+        // Reset default position
+        this.options.position = jQuery.extend({}, this.defaultNoticePosition);
+        
+        // Adjust depending on viewport
+        jQuery.each(this.options.responsivePositions, function (viewport, position) {
+          if (windowDimensions.x <= viewport) {
+            this.options.position = position;
+            return false;
+          }
+        }.bind(this));
+        
+        // Set new padding options
+        this.options.adjustDistance = {
+          top: this.options.position.y,
+          right: this.options.position.x,
+          bottom: this.options.position.y,
+          left: this.options.position.x
+        };
+      };
+      
+      // Adjust paddings when window resizes
+      jQuery(window).on('resize.responsivejBoxNotice-' + this.id, function (ev) { if (this.isOpen) { this._adjustNoticePositon(); } }.bind(this));
+      
+      this.open();
+    },
+    
+    
+    // Triggered when notice was created
+    
+    _onCreated: function ()
+    {
+      // Add color class
+      this.wrapper.addClass('jBox-Notice-color jBox-Notice-' + (this.options.color || 'gray'));
+      
+      // Store position in jBox wrapper
+      this.wrapper.data('jBox-Notice-position', this.options.attributes.x + '-' + this.options.attributes.y);
+    },
+    
+    
+    // Triggered when notice opens
+    
+    _onOpen: function ()
+    {
+      // Adjust position when opening
+      this._adjustNoticePositon();
+      
+      // Loop through notices at same window corner and either move or destroy them
+      jQuery.each(jQuery('.jBox-Notice'), function (index, el)
+      {
+        el = jQuery(el);
+        
+        // Abort if the element is this notice or when it's not at the same position
+        if (el.attr('id') == this.id || el.data('jBox-Notice-position') != this.options.attributes.x + '-' + this.options.attributes.y) return;
+        
+        // Remove notice when we don't wont to stack them
+        if (!this.options.stack) {
+          el.data('jBox').close({ignoreDelay: true});
+          return;
+        }
+        
+        // Get the new margin and add to notices
+        var margin = (el.data('jBoxNoticeMargin') ? parseInt(el.data('jBoxNoticeMargin')) : parseInt(el.css('margin-' + this.options.attributes.y))) + this.wrapper.outerHeight() + (jQuery(window).width() > 500 ? 10 : 5);
+        el.data('jBoxNoticeMargin', margin);
+        el.css('margin-' + this.options.attributes.y, margin);
+        
+      }.bind(this));
+    },
+    
+    
+    // Remove notice from DOM when closing finishes
+    
+    _onCloseComplete: function ()
+    {
+      this.destroy();
+    }
+    
+  });
+  
 });
