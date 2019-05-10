36f2b4421a925fca77bf42f7ffa49b468a14d209
Stephan Wagner <stephanwagner.me@gmail.com>
2019-05-10 18:56:02 +0200

    Fix umd wrapper

--- src/js/umd.js
+++ src/js/umd.js
@@ -10,10 +10,8 @@
   }
 }(this, function (jQuery) {
   var jBox = jBoxWrapper(jQuery);
-  try {
-    window.jBoxConfirmWrapper && window.jBoxConfirmWrapper(jBox, jQuery);
-    window.jBoxImageWrapper && window.jBoxImageWrapper(jBox, jQuery);
-    window.jBoxNoticeWrapper && window.jBoxNoticeWrapper(jBox, jQuery);
-  } catch(e) { console.error(e); }
+  try { jBoxConfirmWrapper && jBoxConfirmWrapper(jBox, jQuery); } catch(e) { console.error(e); }
+  try { jBoxImageWrapper && jBoxImageWrapper(jBox, jQuery); } catch(e) { console.error(e); }
+  try { jBoxNoticeWrapper && jBoxNoticeWrapper(jBox, jQuery); } catch(e) { console.error(e); }
   return jBox;
 }));
